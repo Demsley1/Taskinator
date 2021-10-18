2f06357cdc0ed7d99f2d5fbe8cbb9c0907fb1dea
Denva <emsley3684@gmail.com>
2021-10-18 10:39:24 -0400

    added new function to capture vents and let it stay on the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
-var buttonE1 = document.querySelector("#save-task");
+var formE1 = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoE1.appendChild(listItemEl);
-}
-
-buttonE1.addEventListener ("click", createTaskHandler);
+var createTaskHandler = function(event) {
+  event.preventDefault();
+  
+  var listItemE1 = document.createElement("li");
+  listItemE1.className = "task-item";
+  listItemE1.textContent = "This is a new task.";
+  tasksToDoE1.appendChild(listItemE1);
+};
+  
+formE1.addEventListener("submit", createTaskHandler);
 
 
 
